1040d2ee34fda42dceddc9ff36ad7cbb3899e40d
Oksana-png <oksana_ovchinnikova@inbox.ru>
2022-07-15 17:11:39 +0500

    create header and cre.hygen

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import style from './Header.module.css';
+import Layout from '../Layout';
+import Logo from './Logo';
+import Searh from './Search';
+import Auth from './Auth';
+import Heading from './Heading';
+
+
+export const Header = props => {
+  return (
+    <header className={style.header}>
+      <Layout>
+        <div className={style.gridContainer}>
+          <Logo/>
+          <Heading text="Заголовок" />
+          <Searh/>
+          <Auth auth=""/>
+        </div>
+      </Layout>
+    </header>
+  )
+}
